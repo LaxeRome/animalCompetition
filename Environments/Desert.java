a8086af413897aab41aa8fa1e2791654f8d77d36
Alexander Moore <92391821+LaxeRome@users.noreply.github.com>
2021-10-28 15:56:31 +0000

    Switched the and statements to the or statements. Started working on the environments.

--- Environments/Desert.java
+++ Environments/Desert.java
@@ -1,10 +1,50 @@
+package Environments;
+
 public class Desert extends Environment {
 
   public String environment() {
     return "desert";
   }
 
-  public boolean isWater() {
-    return false;
+  public int adjustedSpeed(String animal, int animalSpeed) {
+    int newSpeed = animalSpeed;
+    switch(animal) {
+    case "crocodile":
+     newSpeed *= 100; // very little chance of avoiding attacks.
+     break;
+    case "hawk": // not impacted by the default effects.
+     break;
+    case "hyena":
+     newSpeed -= 1; // increases chance to avoid.
+     break;
+    case "snake": // not impacted by the default effects.
+     break;
+    default: // every animal has smaller chance to avoid in desert.
+     newSpeed += 5;
+    }
+    return newSpeed;
+  }
+  // this environment has a big bonus to almost every 
+  // animals defense.
+  public double adjustedDefense(String animal, double animalDefense) {
+    double newDefense = animalDefense;
+    switch(animal) {
+      case "crocodile":
+       newDefense -= 0.4; // buffs crocodile defense.
+       break;
+      case "hawk":
+       newDefense += 0.5; // weaker in beach environment.
+       break; 
+      default:
+       newDefense -= 0.2;
+       break;
+    }
+    return newDefense;
+  }
+  // this particular environment has no impact on 
+  // animal stamina.
+  public double adjustedStamina(String animal, double animalStamina) {
+    double newStamina = animalStamina;
+    return newStamina; 
   }
 }
--- Environments/Environment.java
+++ Environments/Environment.java
@@ -1,3 +1,5 @@
+package Environments;
+
 import java.util.*;
   
 
@@ -10,18 +12,22 @@ public abstract class Environment {
   }
 
   public abstract String environment();
+  
+  public abstract int adjustedSpeed(String animal, int animalSpeed);
+  
+  public abstract double adjustedDefense(String animal, double animalDefense);
 
-  public abstract boolean isWater();
+  public abstract double adjustedStamina(String animal, double animalStamina);
 
   public static timeOfDay time() {
     Date time = new Date();
     int currentTime= time.getHours();
     timeOfDay currentHour = timeOfDay.EVENING;
-    if(currentTime >= 19 && currentTime <= 5) {
+    if(currentTime >= 19 || currentTime <= 5) {
       currentHour = timeOfDay.NIGHT;
-    } else if(currentTime >= 6 && currentTime <= 11) {
+    } else if(currentTime >= 6 || currentTime <= 11) {
       currentHour = timeOfDay.MORNING;
-    } else if(currentTime >= 12 && currentTime <= 18) {
+    } else if(currentTime >= 12 || currentTime <= 18) {
       currentHour = timeOfDay.EVENING;
     }
     return currentHour;
